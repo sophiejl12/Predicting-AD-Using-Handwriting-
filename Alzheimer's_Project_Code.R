d2a24f10090d70429e2afc9ca02b28b654dc1605
rmtrivedi <riatrivedi05@gmail.com>
2023-07-19 11:02:07 -0400

    Wrote psudocode

--- Alzheimer's_Project_Code.R
+++ Alzheimer's_Project_Code.R
@@ -1,4 +1,7 @@
-# SETUP ---- 
+###############
+# SETUP ----
+###############
+
 ## Set working directory ----
 getwd()
 
@@ -11,6 +14,38 @@ setwd("/Users/ria/Downloads/Data_mining_project")
 ## Load libraries ----
 library("tidyverse")
 
+
+###############
 # DATA CLEANING ----
+###############
+
 ## Read data ----
 df <- read_csv("Alzheimers_data.csv")
+
+str(df)
+
+## Change class to factor ----
+
+## Drop ID column ----
+
+
+###############
+# MODELING ----
+###############
+
+## Fit initial Random Forest model ----
+
+## Tune Random Forest Model ----
+
+## Fit final rf model ----
+
+## Get accuracy ----
+
+###############
+# FIGURES ----
+
+## Feature importance ----
+
+## Boxplot for most important features vs. class (x-axis) ----
+## Here, either plot the most important features directly (like mean_gmrt_24), 
+## or calculate the mean across all trials for each metric, by group (class = patients/healthy)
