311eae8716a39643aceaf916d7d6824ff313acb2
Sophie Lawrence <sophielawrence1212@gmail.com>
2023-07-19 10:51:29 -0400

    :rocket: Adding Initial code

---
+++ Alzheimer's_Project_Code.R
@@ -0,0 +1,16 @@
+# SETUP ---- 
+## Set working directory ----
+getwd()
+
+## Sophie
+setwd("/Users/sophielawrence/Documents/Data_Mining_Project")
+
+## Ria
+# setwd("/Users/ria/Downloads/DARWIN-2")
+
+## Load libraries ----
+library("tidyverse")
+
+# DATA CLEANING ----
+## Read data ----
+df <- read_csv("Alzheimers_data.csv")
