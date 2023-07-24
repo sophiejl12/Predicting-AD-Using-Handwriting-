bba9caa2aff35d0dda52ffcbb6b51f0068d06003
rmtrivedi <riatrivedi05@gmail.com>
2023-07-24 12:01:49 -0400

    Imported basic mutation function outline

--- Alzheimer's_Project_Code.R
+++ Alzheimer's_Project_Code.R
@@ -28,7 +28,25 @@ str(df)
 df$class <- factor(df$class)
 
 ## Mutating variables 
-
+df <- df %>%
+  mutate(hospital_death = as.factor(hospital_death)) %>%
+  mutate(sepsis = as.factor(sepsis)) %>%
+  mutate(cardiovascular_diagnosis = as.factor(cardiovascular_diagnosis)) %>%
+  mutate(intubated_apache = as.factor(intubated_apache)) %>%
+  mutate(gcs_eyes_apache = as.factor(gcs_eyes_apache)) %>%
+  mutate(gcs_motor_apache = as.factor(gcs_motor_apache)) %>%
+  mutate(temp_apache = as.numeric(temp_apache)) %>%
+  mutate(map_apache = as.numeric(map_apache)) %>%
+  mutate(h1_heartrate_max = as.numeric(h1_heartrate_max)) %>%
+  mutate(d1_resprate_max = as.numeric(d1_resprate_max)) %>%
+  mutate(d1_potassium_max = as.numeric(d1_potassium_max)) %>%
+  mutate(d1_creatinine_max = as.numeric(d1_creatinine_max)) %>%
+  mutate(d1_hematocrit_max = as.numeric(d1_hematocrit_max)) %>%
+  mutate(sodium_apache = as.numeric(sodium_apache)) %>%
+  mutate(wbc_apache = as.numeric(wbc_apache)) %>%
+  mutate(age = as.numeric(age)) %>%
+  mutate(pre_icu_los_days = as.numeric(pre_icu_los_days)) %>%
+  mutate(bmi = as.numeric(bmi))
 
 ## Save cleaned dataset 
 
