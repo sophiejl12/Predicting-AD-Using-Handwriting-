577a15e80d715d1d40c1345a3975b4e08b3bb68c
Sophie Lawrence <sophielawrence1212@gmail.com>
2023-07-24 12:49:45 -0400

    Made basic random forest model and outlines steps to tune model

--- Alzheimer's_Project_Code.R
+++ Alzheimer's_Project_Code.R
@@ -27,31 +27,8 @@ str(df)
 ## Change class to factor ----
 df$class <- factor(df$class)
 
-## Mutating variables 
-df <- df %>%
-  mutate(hospital_death = as.factor(hospital_death)) %>%
-  mutate(sepsis = as.factor(sepsis)) %>%
-  mutate(cardiovascular_diagnosis = as.factor(cardiovascular_diagnosis)) %>%
-  mutate(intubated_apache = as.factor(intubated_apache)) %>%
-  mutate(gcs_eyes_apache = as.factor(gcs_eyes_apache)) %>%
-  mutate(gcs_motor_apache = as.factor(gcs_motor_apache)) %>%
-  mutate(temp_apache = as.numeric(temp_apache)) %>%
-  mutate(map_apache = as.numeric(map_apache)) %>%
-  mutate(h1_heartrate_max = as.numeric(h1_heartrate_max)) %>%
-  mutate(d1_resprate_max = as.numeric(d1_resprate_max)) %>%
-  mutate(d1_potassium_max = as.numeric(d1_potassium_max)) %>%
-  mutate(d1_creatinine_max = as.numeric(d1_creatinine_max)) %>%
-  mutate(d1_hematocrit_max = as.numeric(d1_hematocrit_max)) %>%
-  mutate(sodium_apache = as.numeric(sodium_apache)) %>%
-  mutate(wbc_apache = as.numeric(wbc_apache)) %>%
-  mutate(age = as.numeric(age)) %>%
-  mutate(pre_icu_los_days = as.numeric(pre_icu_los_days)) %>%
-  mutate(bmi = as.numeric(bmi))
-
 ## Save cleaned dataset 
-
-
-
+write.csv(df, file = "Alzheimers_data_cleaned.csv", row.names = FALSE)
 
 
 ###############
@@ -66,16 +43,87 @@ set.seed(123)
 
 ### Split data set into train and test 
 train <- df %>% dplyr::sample_frac(0.80)
-test <- dplyr::anti_join(df, train, by "ID")
+test <- dplyr::anti_join(df, train, by = "ID")
 
 ## Drop ID column ----
 df <- df[, -which(names(df) == "ID")]
 
 ### Separating X and Y 
-train_x <- 
-train_y <- 
+train_x <- test %>% select(-class)
+test_x <- test %>% select(-class)
+train_y <- test %>% select(class)
+test_y <- test %>% select(class)
+test_y <-unlist(test_y)
+train_y <- unlist(train_y)
 
-## Fit final rf model ----
+## Fitting rf model ----
+# Loading random forest 
+library(randomForest)
+rf_model <- randomForest(x = train_x, y = train_y, xtest = test_x, ytest = test_y, importance = TRUE, ntree = 4000)
+
+## Tuning model
+mtry <- tuneRF(
+  x = train_x,
+  y = train_y,
+  xtest = test_x,
+  ytest = test_y,
+  ntreeTry = 400 ,
+  stepFactor =1.5 ,
+  improve =0.01 ,
+  trace =TRUE ,
+  plot = TRUE
+)
+
+# Saving and printing best value tree
+best.m <- mtry[mtry[, 2] == min(mtry[, 2]), 1]
+print(mtry)
+print(best.m)
+
+### Tuning the number of trees
+rf_res_df <-
+  data.frame(
+    TRAINING_ERROR = rf_model$err.rate[,1],
+    ITERATION = c(1:4000)
+  ) %>%
+  mutate(MIN = TRAINING_ERROR == min(TRAINING_ERROR))
+
+best_nrounds <- rf_res_df %>%
+  filter(MIN) %>%
+  pull(ITERATION)
+
+print(best_nrounds)
+
+
+### Fitting final model using best value 
+rf_final_model <-
+  randomForest(
+    x = train_x,
+    y = train_y,
+    mtry = 21,
+    importance = TRUE,
+    ntree = 80
+  )
+rf_final_model
+
+### Feature importance
+
+
+
+# Making features plottable
+
+
+## convert rownames to column
+
+
+## Selecting only relevant columns for mapping
+
+
+# Plotting with ggplot 
+
+
+# Improving plot 
+
+```
 
 ## Get accuracy ----
 
