b641796c18b2a9bfad9bdfefc2b5105168e19c63
rmtrivedi <riatrivedi05@gmail.com>
2023-07-26 12:31:16 -0400

    Update Alzheimer's_Project_Code.R

--- Alzheimer's_Project_Code.R
+++ Alzheimer's_Project_Code.R
@@ -9,14 +9,14 @@ getwd()
 #setwd("/Users/sophielawrence/Documents/Data_Mining_Project")
 
 ##Ria
-# setwd("/Users/ria/Downloads/Data_mining_project")
+setwd("/Users/ria/Downloads/Data_mining_project")
 
 ## Loading libraries ----
 library("tidyverse")
 library("dplyr")
 library(randomForest)
 library(caret)
-#install.packages("rstatix")
+library(dataMaid)
 library(rstatix)
 
 ###############
@@ -133,12 +133,27 @@ rf_features$feature <- rownames(rf_features)
 features <- rf_features %>% dplyr::select(c(feature, rf_imp)) 
 features <- features %>% filter(feature != "ID")
 
+## Keeping only rows with values over 0 
+keep_features <- rf_features %>% select(rf_imp) %>% filter(rf_imp > 0)
+imp_features <- rownames(keep_features)
+
+
+## convert rownames to column
+#rf_features <- as.data.frame(varImp(rf_final_model))
+#rf_features$feature <- rownames(rf_features)
+
+## Selecting only relevant columns for mapping
+#features <- rf_features %>% dplyr::select(c(feature, rf_imp))
+#train_x <- train_x %>% select(all_of(imp_features))
+#test_x <- test_x %>% select(all_of(imp_features))
+
+
 ### Dataset with total importance
 
 # Using stringr to separate the text in the features column, with text in one column "group" and number in another column "num"
 features <- features %>% mutate(group = stringr::str_remove_all(feature, pattern = "\\d+"))
-  
-  
+
+
 # Calculating the total of "rf_imp" by group and the mean of "rf_imp"
 features_grouped <- features %>%
   group_by(group) %>%
@@ -163,14 +178,13 @@ top_10_features$group <- recode(top_10_features$group,
 
 ### Ploting the feature importance
 plot <- top_10_features %>%
-  ggplot(aes(x = total_imp, y = group, color = "#2E86AB", )) +
+  ggplot(aes(x = total_imp, y = group, color = "#2E86AB")) +
   # Creates a point for the feature importance
-  geom_point(position = position_dodge(0.5)) +
-  ggtitle("Top Ten Important Features")
+  geom_point(position = position_dodge(0.5)) 
 
 print(plot)
 
-plot +
+p1 <-plot +
   # Connecting line between 0 and the feature
   geom_linerange(aes(xmin = 0, xmax = total_imp),
                  linetype = "solid",
@@ -188,7 +202,7 @@ plot +
   theme(legend.position = "none",
         text = element_text(family = "serif", face = "bold" )) +
   guides(color = guide_legend(title = NULL)) +
-
+  
   # Plot them in order of importance
   scale_y_discrete(limits = top_10_features$group[order(top_10_features$total_imp, decreasing = FALSE)])
 
@@ -198,17 +212,45 @@ plot +
 library(ggplot2)
 library("patchwork")
 
-# Adding total time and GMRT on paper to original dataset 
+# Adding total time and GMRT on paper to original dataset
 df_total <- df %>% mutate(total_time = (total_time1 + total_time2 + total_time3 + total_time4 + total_time5+total_time6+total_time7+total_time8+total_time9+total_time10+total_time11+total_time12+total_time13+total_time14+total_time15+total_time16+total_time17+total_time18+total_time19+total_time20+total_time21+total_time22+total_time23+total_time24+total_time25))
 summary(df_total)
-         
+
+
+df_total <- df_total %>%
+  group_by(class) %>%
+  mutate(across(where(is.numeric), ~ ifelse(is_extreme(.), mean(., na.rm = TRUE), .)))
+## Get accuracy ----
+
+
+
 ###############
 # FIGURES ----
 
+## Feature importance ----
+
 ## Boxplot for most important features vs. class (x-axis) ----
 
-p2 <- ggplot(data=df, aes(x = class, y = total_time23)) + geom_boxplot()
+p2 <- ggplot(data = df_total3, aes(x = class, y = gmrt_on_paper, fill = class)) + geom_boxplot() + theme_minimal() +theme(legend.position = "none",
+                                                                                                                          text = element_text(family = "serif", face = "bold" )) +
+  guides(color = guide_legend(title = NULL)) +
+  ggtitle("Total gmrt on paper") 
+plot(p2)
 
+p3 <-
+  ggplot(data = df_total, aes(x = total_time, fill = class)) + geom_density(alpha = 0.5) + theme_minimal() + theme(legend.position = "none",
+                                                                                                                   text = element_text(family = "serif", face = "bold" )) +
+  guides(color = guide_legend(title = NULL)) +
+  ggtitle("Total Writing Time of Healthy Participants Compared to Patients")
+plot(p3)
+
+
+
+df_total2 <- df_total %>% select(c(ID, total_time))
+
+# Adding GMRT on paper to original dataset
+df_total3 <- df %>% mutate(gmrt_on_paper = (gmrt_on_paper1 + gmrt_on_paper2 + gmrt_on_paper3 + gmrt_on_paper4 + gmrt_on_paper5+gmrt_on_paper6+gmrt_on_paper7+gmrt_on_paper8+gmrt_on_paper9+gmrt_on_paper10+gmrt_on_paper11+gmrt_on_paper12+gmrt_on_paper13+gmrt_on_paper14+gmrt_on_paper15+gmrt_on_paper16+gmrt_on_paper17+gmrt_on_paper18+gmrt_on_paper19+gmrt_on_paper20+gmrt_on_paper21+gmrt_on_paper22+gmrt_on_paper23+gmrt_on_paper24+gmrt_on_paper25))
+summary(df_total3)
 ## Here, either plot the most important features directly (like mean_gmrt_24),
 
 ## or calculate the mean across all trials for each metric, by group (class = patients/healthy)
